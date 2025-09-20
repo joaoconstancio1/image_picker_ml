1635ccb6ffe04e0cf0e399b15b4dce13b20610ef
joao <joaovitorconstancio@hotmail.com>
2025-09-20 17:11:21 -0300

    Add file selector plugin support and update dependencies
    - Added NDK version to build.gradle.kts
    - Updated Info.plist with usage descriptions for photo library, camera, and microphone
    - Refactored Home widget to StatefulWidget and implemented image selection
    - Registered file selector plugin for Linux, macOS, and Windows
    - Updated pubspec.yaml to include image_picker dependency
    - Updated pubspec.lock with new dependencies

--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,34 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  File? image;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home Screen')),
-      body: Center(child: Column(children: [const Text('Home Screen')])),
+      body: Center(
+        child: Column(
+          children: [
+            image == null
+                ? const Icon(Icons.image, size: 150)
+                : Image.file(image!),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Choose/Capture'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
